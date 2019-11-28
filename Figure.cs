5e9a221d3ef7786a8e5c6185b32f812f26668ae1
vitaminka <veta-sid@mail.ru>
2019-11-28 19:25:43 +0300

    add triangle (doesn't work with button)

--- Figure.cs
+++ Figure.cs
@@ -33,5 +33,41 @@ namespace Paint1
         {
             Brush.DrawLine(startX, startY, endX, endY);
         }
+
+        public void DrawTriangle(int startX, int startY, int endX, int endY)
+        {
+            if (endX > startX)
+            {
+                int a = Convert.ToInt32(Math.Sqrt(Math.Pow(Math.Abs(endX - startX), 2) + Math.Pow(Math.Abs(endY - startY), 2)));
+                int h = Convert.ToInt32(Math.Sqrt(Math.Pow(a, 2) - Math.Pow(startX - (endX - a), 2)));
+                Brush.DrawLine(startX, startY, endX, endY);
+                if (endY > startY)
+                {
+                    Brush.DrawLine(endX, endY, endX - a, startY + h);
+                    Brush.DrawLine(endX - a, startY + h, startX, startY);
+                }
+                else
+                {
+                    Brush.DrawLine(endX, endY, endX - a, startY - h);
+                    Brush.DrawLine(endX - a, startY - h, startX, startY);
+                }
+            }
+            else
+            {
+                int a = Convert.ToInt32(Math.Sqrt(Math.Pow(Math.Abs(endX - startX), 2) + Math.Pow(Math.Abs(endY - startY), 2)));
+                int h = Convert.ToInt32(Math.Sqrt(Math.Pow(a, 2) - Math.Pow(startX - (endX + a), 2)));
+                Brush.DrawLine(startX, startY, endX, endY);
+                if (endY > startY)
+                {
+                    Brush.DrawLine(endX, endY, endX + a, startY + h);
+                    Brush.DrawLine(endX + a, startY + h, startX, startY);
+                }
+                else
+                {
+                    Brush.DrawLine(endX, endY, endX + a, startY - h);
+                    Brush.DrawLine(endX + a, startY - h, startX, startY);
+                }
+            }
+        }
     }
 }

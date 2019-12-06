77632f02f45798f529792b346ae8f98392d0924f
vitaminka <veta-sid@mail.ru>
2019-12-06 13:16:33 +0300

    готовый треугольник

--- figures/Triangle.cs
+++ figures/Triangle.cs
@@ -19,36 +19,52 @@ namespace Paint1
             }
             else
             {
+                int w = Math.Abs(endX - startX);
+                int h = Math.Abs(endY - startY);
+
+                line.Draw(startX, startY, endX, endY, shift);
+
                 if (endX > startX)
                 {
-                    int a = Convert.ToInt32(Math.Sqrt(Math.Pow(Math.Abs(endX - startX), 2) + Math.Pow(Math.Abs(endY - startY), 2)));
-                    int h = Convert.ToInt32(Math.Sqrt(Math.Pow(a, 2) - Math.Pow(startX - (endX - a), 2)));
-                    line.Draw(startX, startY, endX, endY, shift);
                     if (endY > startY)
                     {
-                        line.Draw(endX, endY, endX - a, startY + h, shift);
-                        line.Draw(endX - a, startY + h, startX, startY, shift);
+                        int x3 = Convert.ToInt32(w * Math.Cos(Math.PI / 3) - h * Math.Sin(Math.PI / 3) + startX);
+                        int y3 = Convert.ToInt32(w * Math.Sin(Math.PI / 3) + h * Math.Cos(Math.PI / 3) + startY);
+                        line.Draw(endX, endY, x3, y3, shift);
+                        line.Draw(x3, y3, startX, startY, shift);
                     }
                     else
                     {
-                        line.Draw(endX, endY, endX - a, startY - h, shift);
-                        line.Draw(endX - a, startY - h, startX, startY, shift);
+                        int x3 = Convert.ToInt32(Math.Abs(w * Math.Cos(Math.PI / 3) - h * Math.Sin(Math.PI / 3) - endX));
+                        int y3 = Convert.ToInt32(w * Math.Sin(Math.PI / 3) + h * Math.Cos(Math.PI / 3) + endY);
+                        line.Draw(endX, endY, x3, y3, shift);
+                        line.Draw(x3, y3, startX, startY, shift);
                     }
                 }
                 else
                 {
-                    int a = Convert.ToInt32(Math.Sqrt(Math.Pow(Math.Abs(endX - startX), 2) + Math.Pow(Math.Abs(endY - startY), 2)));
-                    int h = Convert.ToInt32(Math.Sqrt(Math.Pow(a, 2) - Math.Pow(startX - (endX + a), 2)));
-                    line.Draw(startX, startY, endX, endY, shift);
                     if (endY > startY)
                     {
-                        line.Draw(endX, endY, endX + a, startY + h, shift);
-                        line.Draw(endX + a, startY + h, startX, startY, shift);
+                        int x4 = Convert.ToInt32(endX + w * Math.Cos(Math.PI / 3) - h * Math.Sin(Math.PI / 3));
+                        int y4 = Convert.ToInt32(endY - (w * Math.Sin(Math.PI / 3) + h * Math.Cos(Math.PI / 3)));
+                        line.Draw(endX, endY, x4, y4, shift);
+                        line.Draw(x4, y4, startX, startY, shift);
+
                     }
                     else
                     {
-                        line.Draw(endX, endY, endX + a, startY - h, shift);
-                        line.Draw(endX + a, startY - h, startX, startY, shift);
+                        int x4 = Convert.ToInt32(startX + Math.Abs(w * Math.Cos(Math.PI / 3) - h * Math.Sin(Math.PI / 3)));
+                        if (h == 0)
+                        {
+                            x4 = Convert.ToInt32(endX + Math.Abs(w * Math.Cos(Math.PI / 3) - h * Math.Sin(Math.PI / 3)));
+                        }
+                        if (w != 0 && Math.Atan(h / w) <= Math.PI / 6)
+                        {
+                            x4 = Convert.ToInt32(endX + Math.Abs(w * Math.Cos(Math.PI / 3) + h * Math.Sin(Math.PI / 3)));
+                        }
+                        int y4 = Convert.ToInt32(Math.Abs(w * Math.Sin(Math.PI / 3) + h * Math.Cos(Math.PI / 3) - startY));
+                        line.Draw(endX, endY, x4, y4, shift);
+                        line.Draw(x4, y4, startX, startY, shift);
                     }
                 }
             }
